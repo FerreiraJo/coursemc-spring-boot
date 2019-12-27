05b00edaa03002bd3f8eecfb29a95adc3db93021
FerreiraJo <jonathan_silva02@hotmail.com>
2019-12-27 20:55:10 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/jonathanferreira/coursemc/CoursemcApplication.java
+++ src/main/java/com/jonathanferreira/coursemc/CoursemcApplication.java
@@ -1,8 +1,6 @@
 package com.jonathanferreira.coursemc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -10,13 +8,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.jonathanferreira.coursemc.domain.Categoria;
+import com.jonathanferreira.coursemc.domain.Produto;
 import com.jonathanferreira.coursemc.repositories.CategoriaRepository;
+import com.jonathanferreira.coursemc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CoursemcApplication implements CommandLineRunner{
 	
 	@Autowired
 	public CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	public ProdutoRepository produtoRepository;
+	
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CoursemcApplication.class, args);
@@ -28,8 +33,21 @@ public class CoursemcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Mouse", 80.00);
+		Produto p3 = new Produto(null, "Impressora", 800.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+	
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));	
 		
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
---
+++ src/main/java/com/jonathanferreira/coursemc/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.jonathanferreira.coursemc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jonathanferreira.coursemc.domain.Produto;
+
+@Repository  
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
